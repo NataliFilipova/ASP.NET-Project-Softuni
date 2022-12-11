f23cd9593dd2edd8088daff3897eec0cf1361c1e
Natali Filipova <56434467+NataliFilipova@users.noreply.github.com>
2022-12-11 22:10:41 +0200

    testdone

---
+++ BabyKat/BabyKat.Test/Services/CategoryServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using BabyKat.Core.Contracts;
+using BabyKat.Core.Models.Categoryy;
+using BabyKat.Core.Services;
+using BabyKat.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyKat.Test.Services
+{
+    [TestFixture]
+    public class CategoryServiceTest :UnitTestBase
+    {
+        private ICategoryService categoryService;
+        [SetUp]
+        public void SetUp()
+        {
+            this.categoryService = new CategoryService(this.repo);
+        }
+
+        [Test]
+
+        public async Task GetCategoriesAsync()
+        {
+            var categories = dbContext.Categories.Count();
+
+            var result = categoryService.GetCategoriesAsync().Result;
+
+            Assert.AreEqual(categories, result.Count());
+        }
+
+        //[Test]
+        //public async Task RemoveCategory()
+        //{
+        //    var count = dbContext.Categories.Count();
+        //    var categoryTest = dbContext.Categories.Where(p => p.Id == category.Id).FirstOrDefault();
+
+
+        //    categoryService.RemoveCategory(categoryTest.Id);
+        //    var result = dbContext.Categories.Count();
+
+        //    Assert.AreEqual(count - 1, result);
+        //}
+    }
+}
--- BabyKat/BabyKat.Test/Services/CommentServiceTest.cs
+++ BabyKat/BabyKat.Test/Services/CommentServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using BabyKat.Core.Contracts;
+using BabyKat.Core.Models.Commentt;
 using BabyKat.Core.Services;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace BabyKat.Test.Services
 {
+    [TestFixture]
     public class CommentServiceTest :UnitTestBase
     {
         private ICommentService commentService;
@@ -17,6 +19,43 @@ namespace BabyKat.Test.Services
         public void SetUp()
         {
             this.commentService = new CommentService(this.repo);
+        }
+
+        [Test]
+        public async Task Add_Comment()
+        {
+            //Arrange
+            var articleTest = this.dbContext.Articles.Where(u => u.Id == this.article.Id).FirstOrDefault();
+
+            CommentModel commentModel = new CommentModel()
+            {
+                Article = articleTest,
+                Description = "Idiaodwaoihdwaoidhoihwadoihawiodhawoihdawoidhawiohdaodwa",
+                Author = "Ivanka",
+                ArticleId = articleTest.Id
+            };
+
+            //Act
+            commentService.AddComment(commentModel);
+
+            var result = dbContext.Comments.Where(p => p.ArticleId == articleTest.Id).Count();
+
+            //Assert
+            Assert.AreEqual(3, result);
+
+        }
+
+        [Test]
+        public async Task DeleteComment()
+        {
+
+            var articleCommentCount = article.Comments.Count(); 
+
+            var action = commentService.DeleteComment(comment.Id);
+
+            var result = article.Comments.Count();
+
+            Assert.AreEqual(articleCommentCount - 1, result);
         }
 
     }
